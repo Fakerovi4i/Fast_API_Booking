cd60e540999dd1710ad2b4c20dda952dac0c69c3
Vito <fakerovi4i@gmail.com>
2026-07-06 18:22:13 +0300

    Добавлена возможность динамического импорта hotels, rooms, bookings Добавлены фото для отелей, заранее в базе удалены все данные

---
+++ app/importer/utils.py
@@ -0,0 +1,32 @@
+from app.hotels.dao import HotelDAO
+from app.hotels.rooms.dao import RoomDAO
+from app.bookings.dao import BookingDAO
+
+from typing import Iterable
+import json
+from datetime import datetime
+from app.logger import logger
+
+
+TABLE_MODEL_MAP = {
+    "hotels": HotelDAO,
+    "rooms": RoomDAO,
+    "bookings": BookingDAO,
+}
+
+def convert_csv_to_postgres_format(csv_iterable: Iterable):
+    try:
+        data = []
+        for row in csv_iterable:
+            for k, v in row.items():
+                if v.isdigit():
+                    row[k] = int(v)
+                elif k == "services":
+                    row[k] = json.loads(v.replace("'", '"'))
+                elif "date" in k:
+                    row[k] = datetime.strptime(v, "%Y-%m-%d")
+            data.append(row)
+        return data
+    except Exception:
+        logger.error("Cannot convert CSV into DB format", exc_info=True)
+        return []
